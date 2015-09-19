3860b18be9d90e5542b20583b0e7e0745dbb960f
Adrian Gaw <gawadrian@gmail.com>
2015-09-19 18:58:49 -0400

    Chrome Extension Stylized!

--- chrome_extension/js/sleepguardian.js
+++ chrome_extension/js/sleepguardian.js
@@ -1,77 +1,96 @@
 document.addEventListener('DOMContentLoaded', function() {
-  $('#username').val(localStorage.getItem("username"));
-  $('#full_name').val(localStorage.getItem("full_name"));
+  // IDLE
   chrome.idle.onStateChanged.addListener(function(state) {
     console.log(state);
   });
 
-  var checkPageButton = document.getElementById('sendData');
-  checkPageButton.addEventListener('click', function() {
+  if (!_.isEmpty(localStorage.getItem("username"))) {
+    $("#form__container").hide();
+    $("#name").text(localStorage.getItem("full_name"));
+  } else {
+    $("#welcome__container").hide();
+  }
+
+  $('#loginForm').submit(function (evt) {
+    // Login
     chrome.tabs.getSelected(null, function(tab) {
-      var lastSent = parseInt(localStorage.getItem("lastSent")) ? parseInt(localStorage.getItem("lastSent")) : 0;
       var full_name = $('#full_name').val()
       var username = $('#username').val()
+        
+      $.get("http://www.sleepguardian.co/user/" + username)
+        .done(function (data) {
+          // User exists
+          console.log("User exists");
+          $("#form__container").hide();
+          $("#welcome__container").show();
+          $("#name").text(data["full_name"]);
+          localStorage.setItem("username", data["username"]);
+          localStorage.setItem("full_name", data["full_name"]);
 
-      localStorage.setItem("full_name", full_name);
-      localStorage.setItem("username", username);
-      chrome.history.search({"text":"", "maxResults":2147483647, "startTime": lastSent}, function(history) {
-        _.forEach(history, function(history) {
-          // Delete uneeded keys
-          delete history["id"];
-          delete history["title"];
-          delete history["typedCount"];
-          delete history["visitCount"];
-          delete history["url"];
-
-          // Rename keys
-          history["timestamp"] = Math.floor(history["lastVisitTime"]/1000);
-          delete history["lastVisitTime"];
-
-          // Add extra keys
-          history["source"] = "chrome";
-          history["event_type"] = "page_view";
-        });
-        console.log(history);
-        // Check if User Exists
-        $.get("http://www.sleepguardian.co/user/" + username)
-          .done(function (data) {
-            // User exists
-            console.log("User exists");
-            send_history(username, history);
+        })
+        .fail(function (data) {
+          // User does not exist
+          console.log("User does not exist");
+          $.ajax({
+            method: "POST",
+            url: "http://www.sleepguardian.co/user",
+            contentType: "application/json",
+            data: JSON.stringify({ "username": username,
+                    "full_name": full_name })
+          }).done(function (data) {
+            console.log("Created new User");
+            $("#form__container").hide();
+            $("#welcome__container").show();
+            $("#name").text(data["full_name"]);
+            localStorage.setItem("username", data["username"]);
+            localStorage.setItem("full_name", data["full_name"]);
           }).fail(function (data) {
-            // User does not exist
-            console.log("User does not exist");
-            $.ajax({
-              method: "POST",
-              url: "http://www.sleepguardian.co/user",
-              contentType: "application/json",
-              data: JSON.stringify({ "username": username,
-                      "full_name": full_name })
-            }).done(function (data) {
-              console.log("Created new User");
-              send_history(username, history);
-            }).fail(function (data) {
-              console.log("Failed to create user");
-            });
+            console.log("Failed to create user");
           });
-      });
+        });
     });
-  }, false);
-}, false);
+    evt.preventDefault();
+  });
 
-function send_history(username, history)  {
-  if(!_.isEmpty(history)) {
-    $.ajax({
-      method: "POST",
-      url: "http://www.sleepguardian.co/user/" + username + "/timestamps",
-      contentType: "application/json",
-      data: JSON.stringify(history)
-    }).done(function (data) {
-      localStorage.setItem("lastSent", Date.now());
-    }).fail(function (data) {
-      console.log("Data Saving Failed: " + data);
+  $('#sendDataForm').submit(function (evt) {
+    // Send Data
+    var lastSent = parseInt(localStorage.getItem("lastSent")) ? parseInt(localStorage.getItem("lastSent")) : 0;
+    
+    chrome.history.search({"text":"", "maxResults":2147483647, "startTime": lastSent}, function(history) {
+      _.forEach(history, function(history) {
+        // Delete uneeded keys
+        delete history["id"];
+        delete history["title"];
+        delete history["typedCount"];
+        delete history["visitCount"];
+        delete history["url"];
+
+        // Rename keys
+        history["timestamp"] = Math.floor(history["lastVisitTime"]/1000);
+        delete history["lastVisitTime"];
+
+        // Add extra keys
+        history["source"] = "chrome";
+        history["event_type"] = "page_view";
+      });
+      
+      // Send Data
+      if(!_.isEmpty(history)) {
+        $.ajax({
+          method: "POST",
+          url: "http://www.sleepguardian.co/user/" + localStorage.getItem("username") + "/timestamps",
+          contentType: "application/json",
+          data: JSON.stringify(history)
+        }).done(function (data) {
+          localStorage.setItem("lastSent", Date.now());
+        }).fail(function (data) {
+          console.log("Data Saving Failed: " + data);
+        });
+      } else {
+        console.log("No Data to Send");
+      }
     });
-  } else {
-    console.log("No Data to Send");
-  }
-}
+
+    evt.preventDefault();
+  });
+}, false);
