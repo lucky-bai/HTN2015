6cfcd6486eababc4dbdda44762272d3df1333350
bai <bai.li.2005@gmail.com>
2015-09-19 00:58:13 -0400

    Script able to collect chrome timestamps from DB

--- export_chrome_db/export.py
+++ export_chrome_db/export.py
@@ -1,4 +1,29 @@
 # Script to try to read chrome database file
 # Found at C:\Users\Bai\AppData\Local\Google\Chrome\User Data\Default\History
 
-pass
+import sqlite3
+import datetime
+
+def chrome_timestamp_to_unix(x):
+  return int(x * 9.99992778555301 * 10**-7 - 11644379122)
+
+def main():
+  conn = sqlite3.connect('History.db')
+  cur = conn.cursor()
+  cur.execute('SELECT last_visit_time FROM urls')
+  raw_list = cur.fetchall()
+
+  ts_list = []
+  for ts in raw_list:
+    ts_list.append(ts[0])
+
+  ts_list = sorted(ts_list)
+  for ts in ts_list:
+    unix_ts = chrome_timestamp_to_unix(ts)
+    dt = datetime.datetime.fromtimestamp(unix_ts)
+    print dt
+
+# This tool is able to collect all timestamps from chrome history.
+# However, it doesn't account for timezone changes (all timestamps are
+# stored in UTC)
+main()
