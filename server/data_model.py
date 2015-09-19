905e6eb7d45350ba67c5825ae0282c6b72396f22
Michael Tu <tuzhucheng@outlook.com>
2015-09-19 12:01:31 -0400

    Added DB operations and first API

---
+++ server/data_model.py
@@ -0,0 +1,88 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.htn2015
+users_collection = db.users
+timestamps_collection = db.timestamps
+
+
+class Users(object):
+
+    @staticmethod
+    def create(profile):
+        """
+        Create a user.
+        :param profile: {
+            username: string,
+            full_name: string
+        }
+        :returns A tuple of a boolean and a string. The boolean is True if user is created, False otherwise.
+        The string contains the message.
+        """
+        for key in ('username', 'full_name'):
+            if key not in profile:
+                return False, '{} must be specified.'.format(key)
+
+        user = users_collection.find_one({'username': profile['username']})
+        if user:
+            return False, 'username {} already exists.'.format(profile['username'])
+
+        users_collection.insert_one(profile)
+        return True, ''
+
+    @staticmethod
+    def get(username):
+        """
+        Get a user by username.
+        :param username: username.
+        :return: A dictionary containing the user information. None if not found.
+        """
+        user = users_collection.find_one({'username': username})
+        return user
+
+
+class Timestamps(object):
+
+    @staticmethod
+    def upload(username, timestamps):
+        """
+        Upload timestamps for a user.
+        :param username: username.
+        :param timestamps: A list of dictionaries, where each dictionary is a timestamp.
+        {
+            'timestamp': unix timestamp as integer,
+            'source': 'chrome' or 'android',
+            'event_type': e.g. 'foregound' or 'background' for 'android',
+            'subject': webpage url for chrome events, class name for android events
+        }
+        :return: A tuple of a boolean and a string. The boolean is True if timestamps are created, False otherwise.
+        The string contains the message.
+        """
+        user = users_collection.find_one({'username': username})
+        if not user:
+            return False, 'username {} does not exists.'.format(username)
+
+        for timestamp in timestamps:
+            timestamp['username'] = username
+
+        timestamps_collection.insert_many(timestamps)
+
+        return True, ''
+
+    @staticmethod
+    def get_timestamps(username):
+        """
+        Get timestamps for a user.
+        :param username: username.
+        :return: A list of dictionaries, denoting timestamps.
+        """
+        user = users_collection.find_one({'username': username})
+        if not user:
+            # Silent fail by returning empty list
+            return []
+
+        timestamps = timestamps_collection.find({'username': username})
+        for timestamp in timestamps:
+            timestamp.pop('_id')
+
+        return timestamps
--- server/server.py
+++ server/server.py
@@ -1,11 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+import json
+from flask import Flask, request
 
+from data_model import Users, Timestamps
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    user_info = request.get_json(force=True)
+    status, message = Users.create(user_info)
+    # TODO Return different response based on status and message
+    return ''
+
+
+@app.route('/user/<username>/timestamps')
+def upload_timestamps(username):
+    pass
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
