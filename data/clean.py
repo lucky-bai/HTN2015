10e57805a834c641b41c76d8aec1413fd5c1e2fc
Michael Tu <tuzhucheng@outlook.com>
2015-09-20 01:40:31 -0400

    Track new variables

--- data/clean.py
+++ data/clean.py
@@ -1,7 +1,6 @@
-import os
-
 import pandas as pd
 import numpy as np
+
 
 def load(filename):
     records = []
@@ -15,12 +14,15 @@ def load(filename):
                     'geographic_stratum': line[2:4].strip(),
                     'county': line[134:137].strip(),
                     'gender': int(line[145]),
+                    'age': int(line[103:105]),
                     'general_health': int(line[72].strip()) if line[72].strip() else np.nan,
                     'days_not_enough_sleep': int(line[82:85].strip()) if line[82:85].strip() else np.nan,
                     'education': int(line[117]) if line[117].strip() else np.nan,
                     'employment': int(line[118]) if line[118].strip() else np.nan,
                     'income': int(line[119-121]) if line[119-121].strip() else np.nan,
-                    'life_satisfaction': int(line[224]) if line[224].strip() else np.nan
+                    'life_satisfaction': int(line[224]) if line[224].strip() else np.nan,
+                    'physical_health_not_good': int(line[73:75]) if line[73:75].strip() else np.nan,
+                    'mental_health_not_good': int(line[75:77]) if line[75:77].strip() else np.nan
                 }
                 records.append(record)
 
